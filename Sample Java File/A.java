fcf549fbce96b94c16b2e976439183ef8d81899a
suchishree29 <suchishree.jena29@gmail.com>
2017-03-11 17:31:34 -0800

    Create A.java

---
+++ Sample Java File/A.java
@@ -0,0 +1,21 @@
+import java.util.Collection;
+
+public class A {
+ 
+	private int x;
+	 
+	private int[] y;
+	 
+	private Collection<B> b;
+	 
+	private C c;
+	 
+	private Collection<D> d;
+	
+	public void sum(int i){
+		System.out.println("In A.java file");
+	}
+	private int add1(){
+		
+	}
+}
