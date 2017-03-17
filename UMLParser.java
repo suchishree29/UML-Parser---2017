c1407ae484f03abb2fee0c9ff1824656fd41d3c6
suchishree29 <suchishree.jena29@gmail.com>
2017-03-17 03:30:56 -0700

    Create UMLParser.java

---
+++ UMLParser.java
@@ -0,0 +1,17 @@
+import japa.parser.JavaParser;
+import japa.parser.ast.CompilationUnit;
+import japa.parser.ast.body.ClassOrInterfaceDeclaration;
+import japa.parser.ast.body.MethodDeclaration;
+import japa.parser.ast.visitor.VoidVisitorAdapter;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class UMLParser {
+
+
+
+}
