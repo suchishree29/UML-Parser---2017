603a6525250608403d804fcd3949117ff4ad81d5
suchishree29 <suchishree.jena29@gmail.com>
2017-04-14 15:42:47 -0700

    Update UMLParser.java

--- UMLParser.java
+++ UMLParser.java
@@ -1,22 +1,32 @@
 import japa.parser.JavaParser;
 import japa.parser.ast.CompilationUnit;
 import japa.parser.ast.body.ClassOrInterfaceDeclaration;
+import japa.parser.ast.body.ConstructorDeclaration;
+import japa.parser.ast.body.FieldDeclaration;
 import japa.parser.ast.body.MethodDeclaration;
+import japa.parser.ast.body.VariableDeclarator;
+import japa.parser.ast.type.ClassOrInterfaceType;
 import japa.parser.ast.visitor.VoidVisitorAdapter;
 
 import java.io.File;
 import java.io.FileInputStream;
+//import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 
 
 public class UMLParser {
+	
 	static String[] ClassList ;
+	static String[] params;
+	static String[] paramsConstructor;
+	static String class_type;
 	static ArrayList<String> java_class_name = new ArrayList<String>();
 	static ArrayList<String> class_names = new ArrayList<String>();
 	static ArrayList<String> interface_names = new ArrayList<String>();
 	static ArrayList<String> method_names = new ArrayList<String>();
 	static ArrayList<String> field_names = new ArrayList<String>();
+	static ArrayList<String> constructor_names = new ArrayList<String>();
 	static ArrayList<String> modifiers = new ArrayList<String>();
 	static ArrayList<String> rel_class = new ArrayList<String>();
 	static ArrayList<String> params_list = new ArrayList<String>();
@@ -28,7 +38,7 @@ public class UMLParser {
 
 	public static void main(String[] args) throws Exception{
 		
-			String classNames;
+		String classNames;
 		//String input_dir_name = args[0];
 		String input_dir_name = "E:/SJSU/202Paul/202UMLParser/SampleJavaFiles/TestCase5";
 		//System.out.println("Input Path: " + input_dir_name);
@@ -83,61 +93,62 @@ public class UMLParser {
 							e.printStackTrace();
 						}
 					}
-						//visit and print the methods names
-						new MethodVisitor().visit(cu, null);
-						new FieldVisitor().visit(cu, null);
-						new ConstructorTypeVisitor().visit(cu, null);
-						new ClassOrInterfaceVisitor().visit(cu,null);
 				}
 				
 			}
 		}
 		
-		String classNames;
-		
-		String input_dir_name = args[0];
-		//String input_dir_name = "E:/SJSU/202Paul/202UMLParser/SampleJavaFiles";
-		String outfile = args[1];
-		String out_file = input_dir_name + "/" + outfile +".txt";
-		System.out.println("Output File:" + out_file);
-		
-		
-			//Create new file 
-			File f = new File(input_dir_name);
-		
-			//List the files in the input directory
-			File[] ipFiles = f.listFiles();
-		
-			//for every file in ipFiles
 			for(File fList: ipFiles)
 			{
 				if (fList.isFile()) {
 					classNames = fList.getName();
 					ClassList = classNames.split("[.]");
-					System.out.println(classNames);
-          				System.out.println(java_class_name);
+					//System.out.println(ClassList[0]);
+					if (ClassList[1].equals("java")){
+						java_class_name.add(ClassList[0]);	
+						//for (String j:java_class_name){
+							//System.out.println(j);
+						//}
+						String filename = input_dir_name + "/" + classNames;
 					
-					String filename = input_dir_name + "/" + classNames;
-					//System.out.println(filename);
-					}
-					try {
-						
-					}catch (Exception e) {
-						e.printStackTrace();
-					}
-					finally {
 						try {
-							in.close();
-
-						} catch (IOException e) {
-
+							// creates an input stream for the file to be parsed
+							in = new FileInputStream(filename);
+					
+							// parse the file
+							cu = JavaParser.parse(in);
+							System.out.println(cu);
+							JavaParser.setCacheParser(false);
+							
+						
+						}catch (Exception e) {
 							e.printStackTrace();
 						}
+						finally {
+							try {
+								in.close();
+
+							} catch (IOException e) {
+
+								e.printStackTrace();
+							}
+						}
+						//determine if its a class or interface
+
+						//visit and print the methods names
+						new MethodVisitor().visit(cu, null);
+						new FieldVisitor().visit(cu, null);
+						new ConstructorTypeVisitor().visit(cu, null);
+						new ClassOrInterfaceVisitor().visit(cu,null);
+						write();
+
 					}
 					
+				}
 			}
+			sb.append("@enduml\n");
+			classDiagramUML.classUML(sb);
+			System.out.println(sb);	
+
 	}
-
-}
-
 
