b865581c17386c35fbdd98651730cb961e07c47c
suchishree29 <suchishree.jena29@gmail.com>
2017-04-21 23:04:12 -0700

    Update PlantUmlGenerator.java

--- PlantUmlGenerator.java
+++ PlantUmlGenerator.java
@@ -14,9 +14,10 @@ public class PlantUmlGenerator {
 		SourceStringReader reader;
 		try{
 		reader = new SourceStringReader(plantUmlSource.toString());
+		UMLParser umlDiag = new UMLParser();
 
         FileOutputStream output = new FileOutputStream(new File("E:/SJSU/202Paul/202UMLParser/SampleJavaFiles/output.png"));
-
+		//FileOutputStream output = new FileOutputStream(new File(umlDiag.out_file));
         reader.generateImage(output, new FileFormatOption(FileFormat.PNG, false));
         
 		}catch (IOException e ) {
@@ -24,3 +25,4 @@ public class PlantUmlGenerator {
 		}
 	}
 }
+
